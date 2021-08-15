4f2d57dfe3b9a419c2dc28b347f5299816fa59c4
ArthurDante1994 <74523524+ArthurDante1994@users.noreply.github.com>
2021-08-15 08:29:12 -0300

    Terceiro Commit da Atividade 1
    Foi colocado a movimentação via touch no personagem Samson de forma satisfatória

---
+++ Senac_UC7_Atividade01/Assets/Scripts/ScriptsAT01/Move2D.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    private Touch touch;
+    public GameObject createdObject;
+    private Vector2 posTouch;
+    private Vector3 posPlayer;
+    public bool touchingPlayer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //newtestnew();
+        //testfucao();
+        MoveTouchNew();
+        //MovementTouch();
+        //MovementTouchClick();
+    }
+    void testfucao()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = -10f; // select distance = 10 units from the camera
+        Vector3 possicao = mycamera.ScreenToWorldPoint(mousePos);
+        possicao.z = -4.32f;
+        transform.position = possicao;
+        Debug.Log(possicao);
+    }
+    void MoveTouchNew()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+        }
+
+        if (touch.phase == TouchPhase.Began)
+         {
+          posTouch = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane + 5.65f));
+          posPlayer = transform.position;
+          if(posTouch.x > posPlayer.x - 1f && posTouch.x < posPlayer.x + 1f && posTouch.y > posPlayer.y - 1.5f && posTouch.y < posPlayer.y + 1.5f)
+          {
+             touchingPlayer = true;
+          }
+           //touchingPlayer = true;
+         
+         }
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            touchingPlayer = false;
+        }
+
+        if (touchingPlayer)
+        {
+            Vector3 vec = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane + 5.65f));
+            vec.z = -5.8f;
+            transform.position = vec;
+        }
+    }
+    void MovementTouch()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Moved)
+            {
+                transform.position += (Vector3)t.deltaPosition / 100;
+            }
+        }
+    }
+    void MovementTouchClick()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch X = Input.GetTouch(0);
+            Vector3 possicao = Camera.main.ScreenToWorldPoint(X.position);
+            possicao.z = -2.9022f;
+            transform.position = possicao;
+        }
+    }
+    void newtestnew()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 vec = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane + 5.0f));
+                Vector3 oldVec = Camera.main.ScreenToWorldPoint(touch.position);
+                Debug.Log(oldVec + " vs " + vec);
+                //vec.z = 0;
+                Instantiate(createdObject, vec, Quaternion.identity);
+            }
+        }
+    }
+}
